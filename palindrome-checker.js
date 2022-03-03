4ba413c3644074cfd8b284076865460f3a19e153
Eduardo Santos <97476026+EduardoASCS@users.noreply.github.com>
2022-03-03 17:35:48 -0300

    Update palindrome-checker.js

--- palindrome-checker.js
+++ palindrome-checker.js
@@ -1,5 +1,5 @@
 //When I wrote this code, I didn´t know function such as reverse, sort, map.
-//I decided to share this code becouse, even with little knowledge, I got this challenge.
+//I decided to share this code becouse, even with little knowledge (about a week of studies), I got this challenge.
 
 function palindrome(str) {
   const alphanumeric = /[A-Za-z0-9]/g;
