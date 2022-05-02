2177b105e1d8e684f27ca533cb78c19077c5cffe
Eduardo Santos <97476026+EduardoASCS@users.noreply.github.com>
2022-05-02 10:29:40 -0300

    Update palindrome-checker.js

--- palindrome-checker.js
+++ palindrome-checker.js
@@ -1,31 +1,20 @@
-//When I wrote this code, I didn´t know function such as reverse, sort, map.
-//I decided to share this code becouse, even with little knowledge (about a week of studies), I got this challenge.
+//A palindrome is a word or sentence that's spelled the same way both forward and backward, ignoring punctuation, case, and spacing.
 
 function palindrome(str) {
-  const alphanumeric = /[A-Za-z0-9]/g;
-  let strAlphanumeric = str.match(alphanumeric);
+  const onlyLetters = str.match(/[[A-Za-z0-9]/g);
 
-  if(forward(strAlphanumeric).toLowerCase() == backward(strAlphanumeric).toLowerCase()) {
-    return true;
+  const string = onlyLetters
+    .join('')
+    .toLowerCase();
+
+  const stringReversed = onlyLetters
+    .reverse()
+    .join('')
+    .toLowerCase();
+
+  if (string === stringReversed) {
+    return true
   } else {
-    return false;
-  };
-};
-
-const forward = (string) => {
-  let wordForward = "";
-
-  for (let i = 0; i < string.length; i++) {
-    wordForward = wordForward.concat(string[i]);
-  };
-  return wordForward
-};
-
-const backward = (word) => {
-  let wordBackward = "";
-  
-  for (let j = word.length - 1; j >= 0; j--) {
-    wordBackward = wordBackward.concat(word[i]);
-  };
-  return wordBackward;
-};
+    return false
+  }
+}
